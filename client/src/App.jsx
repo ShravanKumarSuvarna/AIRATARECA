0c43409ca7113e5dbd8e5a11a5b8e48ed0aa93c4
ShravanKumarSuvarna <shravankumarsuvarna4@gmail.com>
2025-04-07 13:31:31 +0530

    Changes to HashRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/Landingpage';
 import DashboardPage from './pages/DashboardPage';
 import AboutPage from './pages/AboutPage';
